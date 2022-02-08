7076fd41773dc44bc6773d4e4beb5e244809ca87
Kir Proto Q <kir.proto@ya.ru>
2022-02-08 09:49:07 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Gradient/Yan_SwiftUI_GradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_GradientApp.swift
+//  Yan_SwiftUI_Gradient
+//
+//  Created by KIR Q on 2022.02.08.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_GradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
